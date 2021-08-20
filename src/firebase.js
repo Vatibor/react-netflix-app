57bec3a013af2c64cdb62995ee0945662ec126d5
Vatibor <vatibor2@gmail.com>
2021-08-20 14:46:52 +0200

    create react app, and clean folder to the unnecessary files and codes, start build the components

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDRsWKBaCkuD95aEuG1nH-ofbAm-mFgZfM",
+    authDomain: "react-netflix-app-53e77.firebaseapp.com",
+    projectId: "react-netflix-app-53e77",
+    storageBucket: "react-netflix-app-53e77.appspot.com",
+    messagingSenderId: "242573598962",
+    appId: "1:242573598962:web:806b414a5ca13a5334706b"
+  };
